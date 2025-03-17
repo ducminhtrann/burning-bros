a952ab579dd4b6b2060519f578a86a58335edd46
Minh Tran <minhtrduc@gmail.com>
2025-03-17 09:56:06 +0700

    chore: remove redundant code

--- src/infrastructures/mongodb/models/product.model.ts
+++ src/infrastructures/mongodb/models/product.model.ts
@@ -1,17 +1,6 @@
 import { Prop, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-import { PRODUCT_CONSTANTS } from 'src/constants';
-import { BaseSchema } from './base';
-import { ApiProperty } from '@nestjs/swagger';
-
-@BaseSchema(PRODUCT_CONSTANTS.MODEL_NAME)
 export class Product extends Document {
-  @ApiProperty({
-    type: String,
-    required: true,
-    description: 'Name of Product (English)',
-    example: 'Iphone En',
-  })
   @Prop({ required: true })
   name_vi: string;
 
